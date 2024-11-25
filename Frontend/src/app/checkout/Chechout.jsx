5a7bc5fef29161999e2d0c2a4d602bf381bb70d0
Ayantikaroy002 <ayantikaroy301@gmail.com>
2024-11-25 21:09:25 +0530

    create checkout page

---
+++ Frontend/src/app/checkout/Chechout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Chechoutpage = () => {
+  return (
+    <>
+      <div>
+       <h1> checckout</h1>
+      </div>
+    </>
+  )
+}
+
+export default Chechoutpage
